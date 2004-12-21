08202ca6e746e7a3be1d89ec2013a7bc874b49ca
lieutenant <lieutenant@33272c4e-51e2-0310-a7f1-966f4c758275>
2004-12-21 15:44:27 +0000

    reference file + 'total number of traj.' + new arrangement
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@12 33272c4e-51e2-0310-a7f1-966f4c758275

--- SRC/monitor1.c
+++ SRC/monitor1.c
@@ -1,18 +1,19 @@
-/********************************************************************************************/
-/*  VITESS module 'monitor1.c'                                                              */
-/*    monitoring of intensity as a function of 1 parameter                                  */
-/*                                                                                          */
-/* The free non-commercial use of these routines is granted providing due credit is given   */
-/* to the authors:                                                                          */
-/* 1.0 S. Schorr         1997                                                               */
-/* 1.1 G. Zsigmond   JUL 2002 extended                                                      */
-/* 1.2 G. Zsigmond   MAR 2003 error bars output and option for normalisation with binsize   */
-/*                            included                                                      */
-/* 1.3 K. Lieutenant NOV 2003 monitoring dependent on colour                                */
-/* 1.4 K. Lieutenant JAN 2004 changes for 'instrument.dat'                                  */
-/* 1.5 K. Lieutenant OKT 2004 no abort, if output file cannot be opened; peak flux; count   */
-/*                            rate within binning; protocol; no limit in number of binnings */
-/********************************************************************************************/
+/*********************************************************************************************/
+/*  VITESS module 'monitor1.c'                                                               */
+/*    monitoring of intensity as a function of 1 parameter                                   */
+/*                                                                                           */
+/* The free non-commercial use of these routines is granted providing due credit is given    */
+/* to the authors:                                                                           */
+/* 1.0  S. Schorr         1997                                                               */
+/* 1.1  G. Zsigmond   JUL 2002 extended                                                      */
+/* 1.2  G. Zsigmond   MAR 2003 error bars output and option for normalisation with binsize   */
+/*                             included                                                      */
+/* 1.3  K. Lieutenant NOV 2003 monitoring dependent on colour                                */
+/* 1.4  K. Lieutenant JAN 2004 changes for 'instrument.dat'                                  */
+/* 1.5  K. Lieutenant OKT 2004 no abort, if output file cannot be opened; peak flux; count   */
+/*                             rate within binning; protocol; no limit in number of binnings */
+/* 1.5a K. Lieutenant DEC 2004 total no of trajectories within ...                           */
+/*********************************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,35 +29,41 @@
 
 int main(int argc, char *argv[])
 {
-  FILE	*pFileMon=NULL;
-  char	*MonitorFileName=NULL, sNewName[99]="", sModuleName[41];
+  FILE	*pFileMon=NULL,       /* monitor output file */
+         *pFileRef=NULL;       /* reference file      */
+  char	*MonitorFileName=NULL, 
+         *RefFileName=NULL, 
+         sNewName[99]="", sModuleName[41],
+         sBuffer[512];
   char   sUnit[MAX_KIND+1][ 4]={"", "Ang", "ms", "deg", "deg","cm", "cm"},
          sParN[MAX_KIND+1][22]={"", "wavelength", "time", 
                                 "horizontal divergence", "vertical divergence",
                                 "horizontal position",   "vertical position"};
 
-
-  int	   dy;
-  long	i, kind, exclusivecount, registered, normalise, BufferIndex, nBiny=10, nColour=0 ;
-  double pIntc=0.0, Miny=0.0,p,Maxy=1.0,probactiv, norm, time;
+  short  bProbWeight=0;        /* Probability weight yes or no */
+  long   iBin,                 /* bin number */
+         i, kind, exclusivecount, registered, normalise, BufferIndex, nBiny=10, nColour=0,
+         nTrjTot=0;            /* total number of traj. within binning and eval. time */
+  double dIntTot=0.0,          /* total count rate within binning and eval. time      */
+         Miny=0.0,p,Maxy=1.0, time;
   double Divy, Divz, 
          dEvalTimeMin=-1.0e10, /* min. and max. TOF to be taken into account */
          dEvalTimeMax=1.0e10, 
-         dIntMax=-1.0e10,      /* maximal count rate found in one bin      */
-         dBinSize;             /* size of each bin                         */
+         dIntMax=-1.0e10,      /* maximal count rate found in one bin        */
+         dBinSize;             /* size of each bin                           */
   double dTimeMeas,            /* measuring time     (from simulation.inf, not needed) */
          dLmbdWant,            /* desired wavelength (from simulation.inf, not needed) */
-         dFreq;                /* source frequency   (from simulation.inf) */
-  double *pPosT, /* intensity (=count rate) per bin */
-         *pInt,  /* intensity (=count rate) per bin */
-         *pSD;   /* standard deviation per bin      */
-  long   *pBinN; /* number of trajectories per bin */
+         dFreq;                /* source frequency   (from simulation.inf)   */
+  double *pPosT=NULL,          /* limits of bin (minimal and maximal value)  */
+         *pInt=NULL,           /* intensity (=count rate) per bin  */ 
+         *pNorm=NULL,          /* normalisation value for each bin */
+         *pSD=NULL;            /* standard deviation per bin       */
+  long   *pBinN=NULL;          /* number of trajectories per bin   */
 
 
   /*initialisation*/
   BufferIndex = 0;
   kind = 1;
-  probactiv=1.0;
   normalise = 0;
   p=0.0;
   exclusivecount=0;
@@ -92,6 +99,14 @@ int main(int argc, char *argv[])
         }
         break;
   
+      case 'R':
+        RefFileName=&argv[i][2];
+        pFileRef = fopen(FullParName(RefFileName),"rt");
+        if (pFileRef==NULL)
+        {  fprintf(LogFilePtr,"\nReference file %s could not be opened\n", RefFileName);
+        }
+        break;
+  
       case 'k':
         kind = atol(&argv[i][2]); /* 1= monitorlambda; 2=monitortime; 3=monitordivy, 4=monitordivz, 5=monitory, 6=monitorz */
         break;
@@ -102,6 +117,7 @@ int main(int argc, char *argv[])
         pPosT = (double*) calloc(nBiny+1,sizeof(double));
         pInt  = (double*) calloc(nBiny+1,sizeof(double));
         pSD   = (double*) calloc(nBiny+1,sizeof(double));
+        pNorm = (double*) calloc(nBiny+1,sizeof(double));
         pBinN = (long*)   calloc(nBiny+1,sizeof(long));
 
         //if (nbiny>10000) {fprintf(LogFilePtr,"\n number of bins must be <= 10000"); exit(99);}
@@ -116,16 +132,16 @@ int main(int argc, char *argv[])
         break;
     
       case 'p':
-        probactiv = atof(&argv[i][2]); /* p=1 means probabilities activated,  */
-        break;                         /* else neutron weight is set to 1.0   */
+        bProbWeight = (short) atol(&argv[i][2]); /* p=1 means probability weight activated, */
+        break;                                   /* else neutron weight is set to 1.0       */
 
       case 'e':
         if(argv[i][2]=='1') exclusivecount = 1;   /* if activated, only neutrons meeting the monitor conditions are considered further on */
         break;
     
       case 'f':
-        if(argv[i][2]=='1') normalise = 1; /* if activated, each channel normalised with bin size */
-        break;
+        if(argv[i][2]=='1') normalise = 1; /* if 1, each channel normalised with bin size */
+        break;                             /* if 2, each channel normalised reference file */
     
       case 'C':
         nColour = atol(&argv[i][2]);       /*  excludes all neutrons with diff. Colour, if nColour > 0 */
@@ -151,37 +167,37 @@ int main(int argc, char *argv[])
   if (MonitorFileName==NULL)
     {fprintf(LogFilePtr,"\n you must define a MonitorOutputFile"); exit(99);}
 
-  sprintf(sModuleName, "monitor1_%s 1.5", sParN[kind] );
+  sprintf(sModuleName, "monitor1_%s 1.5a", sParN[kind] );
   print_module_name(sModuleName);
+
+  if (pFileRef!=NULL)
+    normalise=2;
   
   /*initialisation*/
   dBinSize = (Maxy-Miny)/(double)nBiny;
-  for (dy=0; dy<=nBiny; dy++)
+  for (iBin=0; iBin<=nBiny; iBin++)
   {
-    pPosT[dy]=Miny+(dBinSize*dy);
-    pInt [dy]=0.0;
-    pSD  [dy]=0.0;
-    pBinN[dy]=0;
+    pPosT[iBin]=Miny+(dBinSize*iBin);
+    pInt [iBin]=0.0;
+    pSD  [iBin]=0.0;
+    pBinN[iBin]=0;
+
+    if(normalise==1) 
+    { pNorm[iBin] =(Maxy-Miny)/(double)nBiny; 
+    }
+    else if (normalise==2 && ReadLine(pFileRef, sBuffer, sizeof(sBuffer)-1))
+    { sscanf(sBuffer, "%lf", pNorm[iBin]);
+    }
+    else
+    { pNorm[iBin] = 1.0;
+    }
   }
 
-
-/*  switch (kind) 
+  switch (normalise) 
   {
-    case 1: fprintf(LogFilePtr, "Option   : wavelength\n"); break;
-    case 2: fprintf(LogFilePtr, "Option   : time\n"); break;
-    case 3: fprintf(LogFilePtr, "Option   : horizontal divergence\n");break;
-    case 4: fprintf(LogFilePtr, "Option   : vertical divergence\n");break;
-    case 5: fprintf(LogFilePtr, "Option   : horizontal position\n");break;
-    case 6: fprintf(LogFilePtr, "Option   : vertical position\n");break;
-    default:Error("Wrong parameter value for monitor option\n");
-  } */
-
-  if(normalise==1) 
-    norm =(Maxy-Miny)/(double)nBiny; 
-  else 
-    norm = 1;
-  if (norm != 1.0) 
-    fprintf(LogFilePtr, "Norm     : %f\n", norm);
+    case 1: fprintf(LogFilePtr, "Norm     : %f\n", pNorm[0]); break;
+    case 2: fprintf(LogFilePtr, "normalized by %s", RefFileName); break;
+  }
   fprintf(LogFilePtr, "Binning  : %d bins from %10.5f to %10.5f %s\n", nBiny, Miny, Maxy, sUnit[kind]);
   fprintf(LogFilePtr, "File     : %s\n", MonitorFileName);
 
@@ -195,7 +211,7 @@ int main(int argc, char *argv[])
       CHECK;	
       registered=0;
 
-      if(probactiv==1.0) {p = InputNeutrons[i].Probability;}
+      if(bProbWeight==1.0) {p = InputNeutrons[i].Probability;}
       else p=1.0;
 	  
       time = InputNeutrons[i].Time;
@@ -212,24 +228,26 @@ int main(int argc, char *argv[])
       switch (kind) 
       {
       case 1:
-        dy=(int)floor((double)nBiny*(InputNeutrons[i].Wavelength - Miny)/(Maxy-Miny));
+        iBin=(int)floor((double)nBiny*(InputNeutrons[i].Wavelength - Miny)/(Maxy-Miny));
         
-        if(dy>=0 && dy<nBiny && time>=dEvalTimeMin && time<=dEvalTimeMax)
+        if(iBin>=0 && iBin<nBiny && time>=dEvalTimeMin && time<=dEvalTimeMax)
         {
-           pInt [dy] = pInt [dy] + p;
-           pBinN[dy] = pBinN[dy] + 1;
-           pIntc     = pIntc + p;
+           pInt [iBin] += p;
+           pBinN[iBin] += 1;
+           dIntTot   += p;
+           nTrjTot   += 1;
            registered=1;
         }
         break;
 	    
       case 2:
-        dy = (int)floor(nBiny*(InputNeutrons[i].Time - Miny)/(Maxy-Miny));	      
-        if((dy>=0)&&(dy<nBiny))
+        iBin = (int)floor(nBiny*(InputNeutrons[i].Time - Miny)/(Maxy-Miny));	      
+        if((iBin>=0)&&(iBin<nBiny))
         {
-           pInt [dy] = pInt [dy] + p;
-           pBinN[dy] = pBinN[dy] + 1;
-           pIntc     = pIntc + p;
+           pInt [iBin] += p;
+           pBinN[iBin] += 1;
+           dIntTot   += p;
+           nTrjTot   += 1;
            registered=1;
         }
         break;
@@ -240,12 +258,13 @@ int main(int argc, char *argv[])
         if ((InputNeutrons[i].Vector[1]==0.0) && (InputNeutrons[i].Vector[0]==0.0))
           {Divy=0.0;}
         
-        dy = (int)floor(nBiny*(Divy - Miny)/(Maxy-Miny));	      
-        if(dy>=0 && dy<nBiny && time>=dEvalTimeMin && time<=dEvalTimeMax)
+        iBin = (int)floor(nBiny*(Divy - Miny)/(Maxy-Miny));	      
+        if(iBin>=0 && iBin<nBiny && time>=dEvalTimeMin && time<=dEvalTimeMax)
         {
-           pInt [dy] = pInt [dy] + p;
-           pBinN[dy] = pBinN[dy] + 1;
-           pIntc     = pIntc + p;
+           pInt [iBin] += p;
+           pBinN[iBin] += 1;
+           dIntTot   += p;
+           nTrjTot   += 1;
            registered=1;
         }
         break;
@@ -256,35 +275,38 @@ int main(int argc, char *argv[])
         if ((InputNeutrons[i].Vector[2]==0.0) && (InputNeutrons[i].Vector[0]==0.0))
           {Divy=0.0;}
 	    
-        dy = (int)floor(nBiny*(Divz - Miny)/(Maxy-Miny));
+        iBin = (int)floor(nBiny*(Divz - Miny)/(Maxy-Miny));
        
-        if(dy>=0 && dy<nBiny && time>=dEvalTimeMin && time<=dEvalTimeMax)
+        if(iBin>=0 && iBin<nBiny && time>=dEvalTimeMin && time<=dEvalTimeMax)
         { 
-           pInt [dy] = pInt [dy] + p;
-           pBinN[dy] = pBinN[dy] + 1;
-           pIntc     = pIntc + p;
+           pInt [iBin] += p;
+           pBinN[iBin] += 1;
+           dIntTot   += p;
+           nTrjTot   += 1;
            registered=1;
         }
         break;
 
       case 5:
-        dy = (int)floor(nBiny*(InputNeutrons[i].Position[1] - Miny)/(Maxy-Miny));	      
-        if(dy>=0 && dy<nBiny  && time>=dEvalTimeMin && time<=dEvalTimeMax)
+        iBin = (int)floor(nBiny*(InputNeutrons[i].Position[1] - Miny)/(Maxy-Miny));	      
+        if(iBin>=0 && iBin<nBiny  && time>=dEvalTimeMin && time<=dEvalTimeMax)
         {
-           pInt [dy] = pInt [dy] + p;
-           pBinN[dy] = pBinN[dy] + 1;
-           pIntc     = pIntc + p;
+           pInt [iBin] += p;
+           pBinN[iBin] += 1;
+           dIntTot   += p;
+           nTrjTot   += 1;
            registered=1;
         }
         break;
 	    
       case 6:
-        dy = (int)floor(nBiny*(InputNeutrons[i].Position[2] - Miny)/(Maxy-Miny));	      
-        if(dy>=0 && dy<nBiny && time>=dEvalTimeMin && time<=dEvalTimeMax)
+        iBin = (int)floor(nBiny*(InputNeutrons[i].Position[2] - Miny)/(Maxy-Miny));	      
+        if(iBin>=0 && iBin<nBiny && time>=dEvalTimeMin && time<=dEvalTimeMax)
         {
-           pInt [dy] = pInt [dy] + p;
-           pBinN[dy] = pBinN[dy] + 1;
-           pIntc     = pIntc + p;
+           pInt [iBin] += p;
+           pBinN[iBin] += 1;
+           dIntTot   += p;
+           nTrjTot   += 1;
            registered=1;
         }
         break;
@@ -300,28 +322,37 @@ int main(int argc, char *argv[])
 
 my_exit:
   if (pFileMon != NULL)
-  { for (dy = 0; dy < nBiny; dy++)
+  { for (iBin = 0; iBin < nBiny; iBin++)
     {
-      if(pBinN[dy]==0) pBinN[dy]=1;
-      pSD[dy] = pInt[dy]*sqrt(1./(double)pBinN[dy]);
+      if(pBinN[iBin]==0) pBinN[iBin]=1;
+      pSD[iBin] = pInt[iBin]*sqrt(1./(double)pBinN[iBin]);
       fprintf(pFileMon,"%  7.7E\t% 11.7E \t% 11.7E \n",
-                       (pPosT[dy]+pPosT[dy+1])/2.0,(pInt[dy]/norm), pSD[dy]/norm);
-      dIntMax = Max(dIntMax, pInt[dy]);
+                       (pPosT[iBin]+pPosT[iBin+1])/2.0,(pInt[iBin]/pNorm[iBin]), pSD[iBin]/pNorm[iBin]);
+      dIntMax = Max(dIntMax, pInt[iBin]);
     }
 
     fclose(pFileMon);
   }
 
-  if (kind==2)
+  if (kind==2) /* monitor TOF */
   { ReadSimData(&dTimeMeas, &dLmbdWant, &dFreq);
     if (dFreq > 0.0)
       fprintf(LogFilePtr, "Peak flux: %11.4e n/s \n", dIntMax/dBinSize/dFreq);
     else
       fprintf(LogFilePtr, "Peak flux: %11.4e n/s \n", dIntMax/dBinSize);
   }
-  fprintf(LogFilePtr, "total neutron count rate within binning: %11.4e n/s \n\n", pIntc);
+  if (bProbWeight)
+    fprintf(LogFilePtr, "total neutron count rate within binning and eval. time: %11.4e n/s \n\n", dIntTot);
+  else
+    fprintf(LogFilePtr, "total number of traject. within binning and eval. time: %ld\n\n", nTrjTot);
 
   stPicture.eType = (short) kind;
+  if (pPosT!=NULL) free(pPosT);
+  if (pInt !=NULL) free(pInt);
+  if (pNorm!=NULL) free(pNorm);
+  if (pSD  !=NULL) free(pSD); 
+  if (pBinN!=NULL) free(pBinN);
+
   Cleanup(0.0,0.0,0.0, 0.0,0.0);
 
   return(0);
