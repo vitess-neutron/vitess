e0028bb0de705b30088230eeba6407067934e4df
fromme <fromme@33272c4e-51e2-0310-a7f1-966f4c758275>
2010-03-02 10:20:14 +0000

    version 2.10alpha
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@102 33272c4e-51e2-0310-a7f1-966f4c758275

--- GUI/control.tcl
+++ GUI/control.tcl
@@ -563,7 +563,7 @@ proc showBeef {w} {
   frame $w.mbar -relief raised -bd 2 -bg $bgColor
   pack $w.mbar -side top -fill both
 
-  set t "VITESS 2.9"
+  set t "VITESS 2.10alpha"
   set maxModule 40
   set DummyEntry "--inactive--"
 
