7cf6acb719c5bfac22316ea7292ceac5afbd2268
fromme <fromme@33272c4e-51e2-0310-a7f1-966f4c758275>
2011-12-13 16:56:36 +0000

    version 2.11 final
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@217 33272c4e-51e2-0310-a7f1-966f4c758275

--- SRC/init.h
+++ SRC/init.h
@@ -64,6 +64,9 @@ extern gsl_rng * vit_gsl_rng;
 /* compilation of gnuplot. The following lines disable most of them.  */
 #pragma warning(disable: 4018 4056 4244 4305 4761 4756 4996)
 #define _CRT_SECURE_NO_WARNINGS
+# if _MSC_VER <= 1200
+    typedef unsigned int uintptr_t; 
+# endif
 #endif
 
 #endif
