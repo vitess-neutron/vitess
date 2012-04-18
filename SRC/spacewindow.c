dc5c3a113a0950a13d68427dda7dd1a8980bbdaf
houben <houben@33272c4e-51e2-0310-a7f1-966f4c758275>
2012-04-18 15:40:47 +0000

    spacewindow.c: Treat only a given color; limit the window to a given angle phi in the yz-plane; removed the limitation to positive distances when using the previous frame; when using previous frame, the neutron stays unaltered or is removed (former versions changed the neutron position); these changes allow using spacewindow as mask on flat detectors where the x position of a neutron is usually uneual to zero. vitess.tcl: according changes in the GUI
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@234 33272c4e-51e2-0310-a7f1-966f4c758275

--- SRC/spacewindow.c
+++ SRC/spacewindow.c
@@ -11,9 +11,10 @@
 /*	                                 collimator: 0 - from file, 1 - gadolinium, 2 - cadmium, */
 /*                                  3 -Bor10, 4 - Eu, 5 - Silicon, 6 - ideal absorber        */
 /* 2.10  Mar  2004  S. Manoshin     Add "choosing" of material for inner part of collimator  */
-/* 2.21  Jul  2004  S. Manoshin     Corrected some bugs for thick collimator	             */
+/* 2.21  Jul  2004  S. Manoshin     Corrected some bugs for thick collimator                 */
 /* 2.22  Jan  2002  K. Lieutenant   correction:  position after beamstop                     */
 /* 2.23  May  2010  A. Houben       "Rotation" of square window by counter rot of neutron pos*/
+/* 2.24  Apr  2012  A. Houben       Treat only neutrons with a given color and phi angle     */
 /*********************************************************************************************/
 
 #include "init.h"
@@ -65,7 +66,8 @@ double  heightmin,       /* z-coordinate: bottom of rectangular window          
   double Thicknesscoll=0.0;
   double Thicknesscolli=0.0;
   double DistMove=0.0;
-
+  short  TreatColor = -1; // Treat only neutrons with a given color
+  double minPhi=-1.0, maxPhi=-1.0;  //Angle in xz plane
 
 
 /******************************/
@@ -80,7 +82,7 @@ int main(int argc, char *argv[])
 
 	double tempdistsquared;
 	double TimeOF;
-	double NewPositionY, NewPositionZ;
+	double NewPositionY, NewPositionZ, Phi;
 	double CenterX, CenterY, CenterZ, SumProb, TOF3 ;
 
 	Neutron Output;
@@ -101,7 +103,7 @@ int main(int argc, char *argv[])
 	Init(argc, argv, VT_WINDOW);
 	OwnInit(argc, argv);
 
-	print_module_name("Space and Window 2.23");
+	print_module_name("Space and Window 2.24");
 
 	if (TransFileName0 != NULL) trans_file0 = fopen(TransFileName0,"r");
 
@@ -113,7 +115,7 @@ int main(int argc, char *argv[])
 	}
 
 
-	if (DistMove < 0.0)
+	if (DistMove < 0.0 && bOldFrame == FALSE)
 	{
 		fprintf(LogFilePtr,"ERROR: Length of space must be >= 0.0 !!!\n");
 		exit(-1);
@@ -356,11 +358,19 @@ int main(int argc, char *argv[])
 			/* 	Move neutron to window with gravity effect and calculate Time of Flight (ms).   */
 			/****************************************************************************************/
 
-			if (InputNeutrons[i].Vector[0] <= 0.0) continue;
-			if (InputNeutrons[i].Wavelength == 0.0) continue;
-			VelocityReal = (double)(V_FROM_LAMBDA(InputNeutrons[i].Wavelength));
-			if (VelocityReal <= 0.0) continue;
+      if ((TreatColor >= 0) && (InputNeutrons[i].Color != TreatColor)) {
+				Output = InputNeutrons[i];
+				WriteNeutron(&Output);
+        continue;
+      }
 
+      if (bOldFrame==FALSE) {
+			  if (InputNeutrons[i].Vector[0] <= 0.0) continue;
+			  if (InputNeutrons[i].Wavelength == 0.0) continue;
+			  VelocityReal = (double)(V_FROM_LAMBDA(InputNeutrons[i].Wavelength));
+			  if (VelocityReal <= 0.0) continue;
+      } else
+			  Output = InputNeutrons[i];
 
 			if (keygrav == 1)
 			{
@@ -395,6 +405,13 @@ int main(int argc, char *argv[])
 				NewPositionY = InputNeutrons[i].Position[1];
 				NewPositionZ = InputNeutrons[i].Position[2];
 			}
+
+      if (minPhi >= 0 && maxPhi <= 360) {
+        //CartesianToSpherical(InputNeutrons[i].Vector, &TwoTheta, &Phi);
+        //Phi = Phi*180.0/M_PI;
+        Phi	= (double)atan2(InputNeutrons[i].Vector[1], InputNeutrons[i].Vector[2])*180.0/M_PI+180.;
+        if (Phi < minPhi || Phi > maxPhi) continue;
+      }
 
 			if(bCircularWindow==TRUE)
 			{	tempdistsquared =  (NewPositionY - ywincenter)*(NewPositionY - ywincenter)
@@ -446,15 +463,16 @@ int main(int argc, char *argv[])
 				 }
 
 
-				if (bOldFrame==FALSE)
+        if (bOldFrame==FALSE) {
 					InputNeutrons[i].Position[0]=0.0;
+          InputNeutrons[i].Time += (double)TOF3 ;
+				  Output = InputNeutrons[i];
+        } else
+           InputNeutrons[i]=Output;
 
-
-				InputNeutrons[i].Time += (double)TOF3 ;
-				Output = InputNeutrons[i];
 				WriteNeutron(&Output);
-			}
-			else
+      }
+			else /* else, if hitting beamstop or out of window */
 			{
 
 				if (keymaterial0 != 6)
@@ -508,6 +526,7 @@ int main(int argc, char *argv[])
 	{
 		fprintf(LogFilePtr,"No neutrons at the exit of this module \n");
 	}
+  if (TreatColor >= 0) fprintf(LogFilePtr,"Only neutrons with color %hd are treated \n", TreatColor);
 
 	fprintf(LogFilePtr," \n");
 
@@ -603,6 +622,17 @@ void  OwnInit(int argc, char *argv[])
 			case 'T':
 				Thicknesscolli = atof(&argv[i][2]);
 				/* in cm, inner material */
+				break;
+      case 'f':
+				sscanf(&(argv[i][2]),"%hd", &TreatColor);
+				break;
+      case 'p':
+				minPhi = atof(&argv[i][2]);
+				/* in deg, min angle in yz plane */
+				break;
+      case 'P':
+				maxPhi = atof(&argv[i][2]);
+				/* in deg, max angle in yz plane */
 				break;
 
 			default:
