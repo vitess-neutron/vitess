137437c07ea683b25bacfc9b2587cfff1e1db730
lieutenant <lieutenant@33272c4e-51e2-0310-a7f1-966f4c758275>
2012-11-20 10:47:55 +0000

    functions for CSNS moved from src_modchar to new module source_csns
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@329 33272c4e-51e2-0310-a7f1-966f4c758275

--- SRC/src_modchar.c
+++ SRC/src_modchar.c
@@ -234,54 +234,6 @@ double TotalFU(const double _dTemp,  const short  _nSource, const short  _nModTy
 	return(dFUAmpl);
 }
 
-/* dTemp      : [K]  eff. moderator temperature 
-   nModType   :      decoupled POISONED, DECOUPLED unpoisoned, COUPLED
-   dPower     : [W]  average source power                    */          
-double CsnsTotalFU(const double dTemp,   const short  eModType, const double dPower)
-{
-  double dFUAmpl=0.0;;
-
-  s_nSource = CSNS; 
-
-  if (dTemp < 100.0)
-  { 
-    if (eModType==POISONED)
-      dFUAmpl = dPower * 2.7e10/5.0e06/(25.0/50.0);
-	else
-      dFUAmpl = dPower * 2.3e11/5.0e06/(25.0/50.0);
-  }
-  else
-  { 
-    dFUAmpl = dPower * 1.8e11/5.0e06/(25.0/50.0);
-  }
-  return dFUAmpl;
-}
-
-
-/* dLambda: [Ang]  wavelength                          [Ang]
-   dTime  :  [s]   time (after beginning of pulse)     [s]          
-   dPosY  :  [cm]  horinzontal position on the source  [cm]
-   dPosY  :  [cm]  vertical position on the source     [cm]   */
-
-double CsnsModFU(const double dLambda, const double dTime, const double dPosY, const double dPosZ)
-{
-  double dFuA,         // amplitude of the flux    [n/(cm²  sterad Ang]
-         dFu=0.0;      // actualflux value         [n/(cm²s sterad Ang]
-
-  dFuA = stMod[imod].dFUAmpl * Maxwellian(dLambda, stMod[imod].dModTemp);
-
-  if      (stMod[imod].eModType==COUPLED   && stMod[imod].dModTemp < 100.0)
-    dFu = dFuA * PulseShape(dTime, 2.9e-04, 20.0);
-  else if (stMod[imod].eModType==DECOUPLED && stMod[imod].dModTemp > 100.0)
-    dFu = dFuA * PulseShape(dTime, 3.5e-05,  5.0);
-  else if (stMod[imod].eModType==POISONED  && stMod[imod].dModTemp < 100.0)
-    dFu = dFuA * PulseShape(dTime, 4.9e-05,  5.0);
-  else
-    Error("data for chosen CSNS moderator not available");
-
-  return dFu;
-}
-  
 
 double EssModFU(const double _dLambda, const double _dTime, const double _dLength)
 {
