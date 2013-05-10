16edcdda7ed367cdb04fd5303c92f38d62c7096b
lieutenant <lieutenant@33272c4e-51e2-0310-a7f1-966f4c758275>
2013-05-10 11:52:14 +0000

    'read_in' added to makefile
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@382 33272c4e-51e2-0310-a7f1-966f4c758275

--- SRC/mmake.pl
+++ SRC/mmake.pl
@@ -39,7 +39,7 @@ my @Obj = qw(general intersection matrix sample softabort);
 # modules which need TOOL (init general message)
 my @C = qw(ascii2bin monitor1
 	   mon2_div mon2_pos mon2_posdiv mon2_tofwl mon2_wldiv mon2_kdiv mon2_rdiv
-	   mon_brilliance velselect writeout gener_batch lattice_dist
+	   mon_brilliance velselect read_in writeout gener_batch lattice_dist
 	   mirror_coating surface_file gener_bispectral guide_shape spin_reset capture_flux runtime
      fom gener_pipe opt_sim);
 
