360cf64fcdd3f8af8f300ccbc27286ca1a144a31
zendler <zendler@33272c4e-51e2-0310-a7f1-966f4c758275>
2013-10-30 10:01:22 +0000

    Update detector (inclined surface, eff. modifyer) and reflectivity documentation
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@464 33272c4e-51e2-0310-a7f1-966f4c758275

---
+++ SRC/detector.h
@@ -0,0 +1,91 @@
+#ifndef DETECTOR_H
+#define DETECTOR_H
+
+typedef struct
+{
+  double r;
+  int axis;
+  short phimode;  // 0 = const. pixel size; 1 = const Delta phi 
+}
+  CylinderDetectorType;
+
+typedef struct
+{
+  double wallThickness;       // only tube det: thickness of tube walls
+  short  vertTubeOrientation, // vertical (1) or horizontal (0) tube axis
+         rectXsec,            // circular (0) or rectangular (1) tube cross-section
+         tubeshift;           // tube layers shifted against each other
+}
+  TubeDetectorType;
+
+typedef struct
+{
+  double Lambda, Eff;
+}
+  EffData;
+
+
+typedef struct
+{
+  FILE    *pfile;
+  EffData *data;
+  char    *filename;
+  long    maxdata;
+}
+  EffFile;
+
+typedef struct
+{
+  CylinderDetectorType Cyl;
+  TubeDetectorType Tube;
+}
+  DetectorGeomType;
+
+typedef struct 
+{
+  DetectorGeomType DG;
+  VectorType Position;
+  VectorType Direction;
+  double PixelWidth[3];
+  double Resolution[3];  
+  double Thickness, Width, Height;
+  double NLayers, NColumns, NRows;
+  double Theta, Phi, Distance,       // detector coordinates (center) 
+         Phi_n, Theta_n;             // inclination of detector surface w.r.t. position vector
+  double GasPressure, GasTemperature,               // only gas det: gas pressure and temperature
+         SolidAtomDensity, SolidAbsorberthickness,  // only solid det: density (N) and thickness of converter layer
+         EfficiencyMod;                             // modify efficiency
+  int Geom,         // 0: flat,   1: cyl,          2: tube
+      usage,        // 0: normal, 1: monitor only, 2: grid off
+      Absorbertype; // Boron10 (0,2), He3 (1), Li (3) or other (5)               
+  short array,          // first or intermediate part of detector array 
+        detectColor,    // only detect neutrons of certain color; -1 = all neutrons
+        addColor;       // tag detected neutrons by adding addColor to color
+ }
+  DetectorType;
+
+
+
+
+
+/* function pointers */
+short (*NeutronIntersectsDetector)(Neutron *Nin, VectorType ISP[]);
+void (*DetectorSpot)(VectorType SP, VectorType DetSpot);
+
+/* prototypes of local functions */
+short NeutronIntersectsCubeDetector(Neutron *Nin, VectorType ISP[]);
+short NeutronIntersectsCylDetector (Neutron *Nin, VectorType ISP[]);
+short NeutronIntersectsLayer(VectorType dir, VectorType pos, int l, VectorType jISP[]);
+short NeutronIntersectsTube(VectorType dir, VectorType pos, int l, VectorType iISP[], VectorType kISP[]);
+
+void CubeDetSpot    (VectorType SP, VectorType DetSpot);
+void CylinderDetSpot(VectorType SP, VectorType DetSpot);
+
+void OwnInit   (int argc, char *argv[]);
+void OwnCleanup();
+void CheckAndAdjustDetectorInput(int type);
+
+double GetXsec(int h_absorbertype, double h_lambda);
+double GetLambdaProbFromEff(const double lambda, const TotalID NeutronID);
+
+#endif
