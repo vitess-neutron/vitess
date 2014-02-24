aa54dc2b88ac471efbe3148d8f4143eba7e1ed26
fromme <fromme@33272c4e-51e2-0310-a7f1-966f4c758275>
2014-02-24 10:01:32 +0000

    version 3.2 release candidate 6
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@589 33272c4e-51e2-0310-a7f1-966f4c758275

--- TOOLS/copycontents.pl
+++ TOOLS/copycontents.pl
@@ -3,10 +3,19 @@
 use File::Copy;
 use strict;
 
-my ($d, $subn, @Files, @Sub);
+my ($d, $subn, $testonly, $sourcedir, $targetdir, $verbose, @Files, @Sub);
 
-my $sourcedir = shift;
-my $targetdir = shift;
+while ($_ = shift) {
+    if (/^-t/) {
+	$testonly = 1;
+    } elsif (/^-v/) {
+	$verbose = 1;
+    } elsif ($sourcedir) {
+	$targetdir = $_;
+    } else {
+        $sourcedir = $_;
+    }
+}
 
 $sourcedir =~ s/\/+$//;
 $targetdir =~ s/\/+$//;
@@ -14,9 +23,10 @@ $targetdir =~ s/\/+$//;
 die("give source directory") unless -d $sourcedir;
 die("give target directory") if $targetdir eq '';
 
-mkdir $targetdir, 0755 unless -d $targetdir;
-
-die("could not create target directory $targetdir;") unless -d $targetdir;
+unless ($testonly) {
+    mkdir $targetdir, 0755 unless -d $targetdir;
+    die("could not create target directory $targetdir;") unless -d $targetdir;
+}
 
 my @D = ($sourcedir);
 while ($d = shift @D) {
@@ -41,11 +51,41 @@ while ($d = shift @D) {
 }
 
 foreach $d (@Sub) {
-  mkdir "$targetdir/$d", 0755;
-  # print "mkdir 0700, $targetdir/$d\n";
+    $_ = "$targetdir/$d";
+    next if -d $_;
+    if ($testonly) {
+	print "would mkdir $_\n";
+    } else {
+	print "mkdir $_\n" if $verbose;
+	mkdir $_, 0755;
+    }
 }
 
 foreach (@Files) {
-  copy("$sourcedir/$_", "$targetdir/$_");
-  # print "$sourcedir/$_ $targetdir/$_\n";
+  mycopy("$sourcedir/$_", "$targetdir/$_");
+}
+
+sub mycopy {
+    my ($a, $b) = @_;
+    return unless -s $a;
+    my $doit;
+    if (-s $b) {
+	# do not overwrite, if same contents
+	my @A = stat $a;
+	my @B = stat $b;
+	if ($A[7] != $B[7]) {
+	    $doit = 1;
+	} else {
+	    $doit = `diff -q $a $b` ? 1 : 0;
+	}
+    } else {
+	$doit = 1;
+    }
+    return unless $doit;
+    if ($testonly) {
+	print "would copy $a, $b\n";
+    } else {
+	print "copy $a $b\n" if $verbose;
+	copy($a, $b);
+    }
 }
