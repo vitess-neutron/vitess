28d4fc93577369f879c3edb37ccce515c18b7659
nekrassov <nekrassov@33272c4e-51e2-0310-a7f1-966f4c758275>
2014-04-09 13:11:24 +0000

    Bug fixed for reflectivity calculation in the case of "other" file format.
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@594 33272c4e-51e2-0310-a7f1-966f4c758275

--- SRC/sm_ensemble_parallel.c
+++ SRC/sm_ensemble_parallel.c
@@ -870,7 +870,11 @@ static double CollideWall
 	// McStas reflection model:
 	// Refl[index_theta] = R0 * 0.5*(1.0-tanh((Q-M2*Qc[l][index_theta])/W)) * (1.0 - alphaQ*(Q-Qc[l][index_theta]) + betaQ*(Q-Qc[l][index_theta])*(Q-Qc[l][index_theta]));
 
+	if (fileFormat == 0) mNumber[l][index_theta] = thetaCSM[l][index_theta]/thetaC[l][index_theta];
+
 	Refl[index_theta] = ReflSN(WL, the*180./M_PI, mNumber[l][index_theta]);
+
+	//	if (fileFormat == 0) Refl[index_theta] = RthetaCSM[l][index_theta] + (1. - RthetaCSM[l][index_theta])/(thetaCSM[l][index_theta] * WL - thetaC[l][index_theta] * WL)*(thetaCSM[l][index_theta] * WL - the);
 
 	if (Choice < Refl[index_theta])
 	  dir[0] *= -1.;
