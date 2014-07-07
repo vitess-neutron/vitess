3cc4a31e93ad2c740e6add2a9e2dc609c52b6a04
houben <houben@33272c4e-51e2-0310-a7f1-966f4c758275>
2014-07-07 15:51:41 +0000

    Bugfix(?) in new calculation of DivZ (since rev. 545). Why has DivY two cases for .Vector[0]>=0 and .Vector[0]<0 but DivZ does not?
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@605 33272c4e-51e2-0310-a7f1-966f4c758275

--- SRC/mon2_posdiv.c
+++ SRC/mon2_posdiv.c
@@ -205,7 +205,7 @@ DECLARE_ABORT;
 	  div_*=180.0/M_PI;
 	  }
 	  else {
-	    div_ = (double) atan2(InputNeutrons[i].Vector[1], sqrt(sq(InputNeutrons[i].Vector[0]) + sq(InputNeutrons[i].Vector[1])));	 
+	    div_ = (double) atan2(InputNeutrons[i].Vector[2], sqrt(sq(InputNeutrons[i].Vector[0]) + sq(InputNeutrons[i].Vector[1])));	 
 	    div_*=180.0/M_PI;
 	  }
 	  dpos = (int)floor(nbin_pos*(pos_-pos_min)/(pos_max-pos_min));
