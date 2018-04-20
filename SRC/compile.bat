cb9935e2e0f449241016936f1b7689739fe2ca07
dmf <dmf@33272c4e-51e2-0310-a7f1-966f4c758275>
2018-04-20 09:27:18 +0000

    Version 3.4 release candidate 1
    git-svn-id: https://www.helmholtz-berlin.de/svn/vitess/trunk@637 33272c4e-51e2-0310-a7f1-966f4c758275

--- SRC/compile.bat
+++ SRC/compile.bat
@@ -1,2 +1,4 @@
-Path=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\BIN
+#use cmd.exe for VS 2017 to set environment
+# old path settings for VS 2010 had been
+# C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\BIN
 nmake /f vitess.mak all
