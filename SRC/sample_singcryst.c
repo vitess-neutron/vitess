dc9ac3599ace782d987d4fb91d12357162969f2d
Klaus Lieutenant <lieuten@iffw2k.kfa-juelich.de>
2023-08-02 19:51:49 +0200

    Color from ID corrected: modulo max. value for a short integer

--- SRC/sample_singcryst.c
+++ SRC/sample_singcryst.c
@@ -264,7 +264,7 @@ int main(int argc, char **argv)
             CopyVector(Pos2v, Neutrons.Position);
             CopyVector(Dir, Neutrons.Vector);
 
-            Neutrons.Color = (short) no[repet]; 
+            Neutrons.Color = (short) (no[repet] % 32768); 
 
             /*	 writes output binary file */
             if (Prob > wei_min) 
